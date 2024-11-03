b9455e134ef2ab3e2c58ece1329c29de96a791fa
Pavel Lozhkin <lozhkin.pavel@gmail.com>
2024-11-03 17:30:20 +0300

    module 0

--- minitorch/module.py
+++ minitorch/module.py
@@ -31,13 +31,15 @@ class Module:
 
     def train(self) -> None:
         """Set the mode of this module and all descendent modules to `train`."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = True
+        for _, module in self._modules.items():
+            module.train()
 
     def eval(self) -> None:
         """Set the mode of this module and all descendent modules to `eval`."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = False
+        for _, module in self._modules.items():
+            module.eval()
 
     def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
         """Collect all the parameters of this module and its descendents.
@@ -47,13 +49,26 @@ class Module:
             The name and `Parameter` of each ancestor parameter.
 
         """
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        res = []
+        for name, param in self._parameters.items():
+            res.append((name, param))
+
+        for name, module in self._modules.items():
+            params = module.named_parameters()
+            for i in range(len(params)):
+                params[i] = (f'{name}.' + params[i][0], params[i][1])
+            res += params
+        return res
 
     def parameters(self) -> Sequence[Parameter]:
         """Enumerate over all the parameters of this module and its descendents."""
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        res = []
+        for _, param in self._parameters.items():
+            res.append(param)
+
+        for _, module in self._modules.items():
+            res += module.parameters()
+        return res
 
     def add_parameter(self, k: str, v: Any) -> Parameter:
         """Manually add a parameter. Useful helper for scalar parameters.
--- minitorch/operators.py
+++ minitorch/operators.py
@@ -1,54 +1,107 @@
 """Collection of the core mathematical operators used throughout the code base."""
 
 import math
-
-# ## Task 0.1
 from typing import Callable, Iterable
 
-#
-# Implementation of a prelude of elementary functions.
-
-# Mathematical functions:
-# - mul
-# - id
-# - add
-# - neg
-# - lt
-# - eq
-# - max
-# - is_close
-# - sigmoid
-# - relu
-# - log
-# - exp
-# - log_back
-# - inv
-# - inv_back
-# - relu_back
-#
-# For sigmoid calculate as:
-# $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
-# For is_close:
-# $f(x) = |x - y| < 1e-2$
+def mul(x: float, y: float) -> float:
+    return x * y
 
 
-# TODO: Implement for Task 0.1.
+def id(x: float) -> float:
+    return x
 
 
-# ## Task 0.3
-
-# Small practice library of elementary higher-order functions.
-
-# Implement the following core functions
-# - map
-# - zipWith
-# - reduce
-#
-# Use these to implement
-# - negList : negate a list
-# - addLists : add two lists together
-# - sum: sum lists
-# - prod: take the product of lists
+def add(x: float, y: float) -> float:
+    return x + y
 
 
-# TODO: Implement for Task 0.3.
+def neg(x: float) -> float:
+    return -x
+
+
+def lt(x: float, y: float) -> float:
+    return 1.0 if x < y else 0.0
+
+
+def eq(x: float, y: float) -> float:
+    return 1.0 if x == y else 0.0
+
+
+def max(x: float, y: float) -> float:
+    return x if x >= y else y
+
+
+def is_close(x: float, y: float) -> float:
+    return 1.0 if abs(x - y) < 1e-2 else 0.0
+
+
+def sigmoid(x: float) -> float:
+    return 1.0 / (1.0 + math.exp(-x)) if x >= 0 else math.exp(x) / (1.0 + math.exp(x))
+
+
+def relu(x: float) -> float:
+    return x if x >= 0 else 0.0
+
+
+def log(x: float) -> float:
+    return math.log(x + 1e-6)
+
+
+def exp(x: float) -> float:
+    return math.exp(x)
+
+
+def log_back(x: float, d: float) -> float:
+    return d / x
+
+
+def inv(x: float) -> float:
+    return 1 / x
+
+
+def inv_back(x: float, d: float) -> float:
+    return -d / x**2
+
+
+def relu_back(x: float, d: float) -> float:
+    return d if x >= 0 else 0.0
+
+
+def map(fn: Callable[[float], float]) -> Callable[[Iterable[float]], Iterable[float]]:
+    return lambda ls: [fn(x) for x in ls]
+
+
+def negList(ls: Iterable[float]) -> Iterable[float]:
+    return map(neg)(ls)
+
+
+def zipWith(
+    fn: Callable[[float, float], float]
+) -> Callable[[Iterable[float], Iterable[float]], Iterable[float]]:
+    
+    return lambda ls1, ls2: [fn(ls1[i], ls2[i]) for i in range(len(ls1))]
+
+
+def addLists(ls1: Iterable[float], ls2: Iterable[float]) -> Iterable[float]:
+    return zipWith(add)(ls1, ls2)
+
+
+def reduce(
+    fn: Callable[[float, float], float], start: float
+) -> Callable[[Iterable[float]], float]:
+    
+    def f(ls: Iterable[float]) -> float:
+        res = start
+        for val in ls:
+            res = fn(res, val)
+        return res
+    return f
+
+
+def sum(ls: Iterable[float]) -> float:
+    return reduce(add, 0.0)(ls)
+
+
+def prod(ls: Iterable[float]) -> float:
+    return reduce(mul, 1.0)(ls)
+
